aa3e440a450beebbcd297dbbfec8522d91941506
bredele <olivier.wietrich@gmail.com>
2014-01-28 19:43:11 -0700

    interact return a configured app

--- examples/basic/index.js
+++ examples/basic/index.js
@@ -5,14 +5,17 @@
 
 var express = require('express'),
 		app = module.exports = express(),
-		interact = require('../../lib').listen(3000); //we should pass socket, app or port
+		interact = require('../../lib').listen(app); //we should pass socket, port in second arg
 
 
+// settings
+
+app.set('views', __dirname);
+app.set('view engine', 'jade');
 
 // middleware
 
 app.use(express.logger('dev'));
-
 
 // mount
 
