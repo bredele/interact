d71433e723b588d7410dfb849ca75297a5e74002
bredele <olivier.wietrich@gmail.com>
2014-01-28 18:44:49 -0700

    refactoring

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 example: node_modules components
-	./bin/examples.io -e basic
+	./bin/examples.io -p 8000 -e basic
 
 node_modules: package.json
 	@npm install
--- examples/basic/index.js
+++ examples/basic/index.js
@@ -5,7 +5,7 @@
 
 var express = require('express'),
 		app = module.exports = express(),
-		interact = require('../../lib').listen(app);
+		interact = require('../../lib').listen(3000); //we should pass socket, app or port
 
 
 
